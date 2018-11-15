f4322c066edb09e07571237064f27f3ce1b0f115
wujianhui <wujianhui@itcast.cn>
2018-11-15 08:41:27 +0800

    提交测试代码

---
+++ test1/src/main/java/com/itheimatest/Demo.java
@@ -0,0 +1,12 @@
+package com.itheimatest;
+
+/**
+ * Date:2018/11/15
+ * Author:JianHui Wu a developer of java
+ * Desc:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%");
+    }
+}
